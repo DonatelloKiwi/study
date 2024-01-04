cf8ffb4e0ed03743cce5031deccc8a0ece195887
Илья Каранинский <karaninsky19@gmail.com>
2024-01-04 23:05:58 +0500

    доавбил калькулятор возраста

---
+++ birth_calculator.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+def calculate_age(birth_year, birth_month, birth_day):
+    current_date = datetime.now()
+    birth_date = datetime(birth_year, birth_month, birth_day)
+
+    age = current_date.year - birth_date.year - ((current_date.month, current_date.day) < (birth_date.month, birth_date.day))
+
+    return age
+
+# Ввод даты рождения от пользователя
+birth_year = int(input("Введите год рождения: "))
+birth_month = int(input("Введите месяц рождения: "))
+birth_day = int(input("Введите день рождения: "))
+
+# Вычисление возраста
+age = calculate_age(birth_year, birth_month, birth_day)
+
+# Вывод результата
+print(f"Ваш возраст: {age} лет")
